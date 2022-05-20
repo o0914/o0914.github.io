fc3a6b84310b715f518c8a99486ef7e097b98789
o0914 <91249729+o0914@users.noreply.github.com>
2022-05-20 10:06:02 +0800

    Update food.js

--- food/food.js
+++ food/food.js
@@ -15,9 +15,15 @@ function draw() {
         rect(50, 100, 150, 2);
     if (choice == 2)
         rect(50, 150, 150, 2);
-    text("Choice 1", 50, 50);
-    text("Choice 2", 50, 100);
-    text("Choice 3", 50, 150);
+    if (choice == 3)
+        rect(50, 200, 75, 2);
+    if (choice == 4)
+        rect(50, 250, 75, 2);
+    text("泡麵", 50, 50);
+    text("Ubereats", 50, 100);
+    text("Foodpanda", 50, 150);
+    text("牛排",50,200);
+    text("拉麵",50,250);
 }
 
 function mousePressed() {
